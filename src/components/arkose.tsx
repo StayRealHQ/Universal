82806226cedbf221be10326f85778ee20ad5dd25
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-02-20 00:57:29 +0100

    fix: fingerprint data response for captcha

--- src/components/arkose.tsx
+++ src/components/arkose.tsx
@@ -1,4 +1,6 @@
-export const createArkoseURL = (key: string, dataExchange: string) => {
+import { BEREAL_IOS_BUNDLE_ID, BEREAL_IOS_VERSION, BEREAL_PLATFORM_VERSION } from "~/api/constants";
+
+export const createArkoseURL = (key: string, dataExchange: string, deviceId: string) => {
   const href = window.location.origin + window.location.pathname;
 
   const html = `<html><head><meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1,user-scalable=0"><style>html,body{display:flex;justify-content:center;align-items:center;background:#000;height:100%;width:100%;overflow:hidden;position:fixed;margin:0;padding:0;color:#fff}.spin{transition: opacity .175s; animation: spin 1s linear infinite}@keyframes spin{to{transform:rotate(360deg)}}</style>`
@@ -17,6 +19,25 @@ enforcement.setConfig({
   },
   onShow(){
     document.querySelector('.spin').style.opacity = 0
+  },
+  onDataRequest(){
+    const p="mobile_sdk__"
+    enforcement.dataResponse(btoa(JSON.stringify({
+      [p+"app_version"]:${JSON.stringify(BEREAL_IOS_VERSION)},
+      [p+"battery_capacity"]:100,
+      [p+"manufacturer"]:"Apple",
+      [p+"app_id"]:${JSON.stringify(BEREAL_IOS_BUNDLE_ID)},
+      [p+"app_signing_credential"]:"",
+      [p+"device_orientation"]:"Un",
+      [p+"brand"]:"Apple",
+      [p+"userAgentModified"]:"",
+      [p+"battery_status"]:"Full",
+      [p+"id_for_vendor"]:${JSON.stringify(deviceId)},
+      [p+"build_version"]:"2.4.0(2.4.0)",
+      [p+"os_version"]:${JSON.stringify(BEREAL_PLATFORM_VERSION)},
+      [p+"screen_brightness"]:100,
+      [p+"storage_info"]:[]
+    })))
   }
 })
 }</script>`
