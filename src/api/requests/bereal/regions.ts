165b1fb145994725036b02b9c9e818022b7aea60
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-06 02:52:16 +0100

    refactor(api): regions listing

--- src/api/requests/bereal/regions.ts
+++ src/api/requests/bereal/regions.ts
@@ -2,12 +2,15 @@ import { BEREAL_DEFAULT_HEADERS } from "../../constants";
 import { fetch } from "@tauri-apps/plugin-http";
 import auth from "../../../stores/auth";
 
-export type MomentsRegions = Array<{
+export type GetBerealRegions = Array<{
   code: string
   name: string
 }>
 
-export const moments_regions = async (): Promise<MomentsRegions> => {
+/**
+ * Retrieve a list of available regions.
+ */
+export const getBerealRegions = async (): Promise<GetBerealRegions> => {
   const response = await fetch("https://mobile-l7.bereal.com/api/bereal/regions?language=en", {
     headers: {
       ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
@@ -18,7 +21,7 @@ export const moments_regions = async (): Promise<MomentsRegions> => {
   // if token expired, refresh it and retry
   if (response.status === 401) {
     await auth.refresh();
-    return moments_regions();
+    return getBerealRegions();
   }
 
   return response.json();
