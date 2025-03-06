b97ca7c89ef485b1e43107b09469f6f40cd4cc8a
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-06 02:52:04 +0100

    feat(api): type all code for terms and be able to update a term status

--- src/api/requests/terms.ts
+++ src/api/requests/terms.ts
@@ -1,19 +1,45 @@
 import { fetch } from "@tauri-apps/plugin-http";
 import { BEREAL_DEFAULT_HEADERS } from "../constants";
 import auth from "../../stores/auth";
+import { BeRealError } from "../models/errors";
 
-export interface Terms {
-  data: Array<{
-    code: string
-    status: "ACCEPTED" | "UNKNOWN" | "DECLINED"
-    signedAt: string
-    termUrl: string
-    version: string
-  }>
+export type TermCode = (
+  | "gps"
+  | "memories"
+  | "terms"
+  | "privacy"
+  | "camera"
+  | "contacts"
+  | "microphone"
+  | "apple-music"
+  | "photo-library-read"
+  | "photo-library-write"
+  | "screen-recording"
+  | "push-notifications"
+  | "show-friends-to-friends" // = Settings > Friends of Friends > "Connect with friends"
+);
+
+export type TermStatus = (
+  | "UNKNOWN"
+  | "ACCEPTED"
+  | "DECLINED"
+);
+
+export type Term = {
+  code: TermCode
+  status: "ACCEPTED" | "UNKNOWN" | "DECLINED"
+  signedAt: string
+  termUrl: string
+  version: string
 }
 
-export const terms = async (): Promise<Terms> => {
+export interface GetTerms {
+  data: Array<Term>
+}
+
+export const getTerms = async (): Promise<GetTerms> => {
   const response = await fetch("https://mobile-l7.bereal.com/api/terms", {
+    method: "GET",
     headers: {
       ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
       authorization: `Bearer ${auth.store.accessToken}`
@@ -23,7 +49,31 @@ export const terms = async (): Promise<Terms> => {
   // if token expired, refresh it and retry
   if (response.status === 401) {
     await auth.refresh();
-    return terms();
+    return getTerms();
+  }
+
+  return response.json();
+};
+
+export const putTerms = async (code: TermCode, status: TermStatus): Promise<Term> => {
+  const response = await fetch(`https://mobile-l7.bereal.com/api/terms/${code}`, {
+    method: "PUT",
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
+      authorization: `Bearer ${auth.store.accessToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ status })
+  });
+
+  // if token expired, refresh it and retry
+  if (response.status === 401) {
+    await auth.refresh();
+    return putTerms(code, status);
+  }
+
+  if (response.status !== 200) {
+    throw new BeRealError(`Failed to update terms for '${code}'`);
   }
 
   return response.json();
