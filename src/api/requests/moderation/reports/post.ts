02f6da7bfe17e1e6f70029f19b74b34df09417d9
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-09 13:38:16 +0100

    feat(api): add post reporting route with all reasons

---
+++ src/api/requests/moderation/reports/post.ts
@@ -0,0 +1,57 @@
+import { fetch } from "@tauri-apps/plugin-http";
+import { BEREAL_DEFAULT_HEADERS } from "~/api/constants";
+import { BeRealError } from "~/api/models/errors";
+import auth from "~/stores/auth";
+
+export enum PostReportReason {
+  // NOTE: these are not used anymore.
+  // OLD_INNAPROPRIATE = "inappropriate",
+  // OLD_UNDESIRABLE = "undesirable",
+
+  Spam = "spam",
+  ScamOrUntrue = "scam-or-untrue", // = "Scam or untrue information"
+  InappropriateCaption = "inappropriate-caption",
+  JustNotForMe = "just-not-for-me",
+  NudityOrSexual = "nudity-or-sexual",
+  ViolentOrDangerous = "violent-or-dangerous",
+  HateSpeechOrSymbols = "hate-speech-or-symbols",
+  SuicideOrSelfHarm = "suicide-or-self-harm",
+  Other = "other" // = "Something else"
+}
+
+/**
+ * report a post for moderation.
+ * @returns a report id
+ */
+export const postModerationReportsPost = async (placement: "feed", reason: PostReportReason, postId: string, comment: string): Promise<string> => {
+  const response = await fetch(`https://mobile-l7.bereal.com/api/moderation/reports/post`, {
+    method: "POST",
+    headers: {
+      ...BEREAL_DEFAULT_HEADERS(auth.store.deviceId),
+      authorization: `Bearer ${auth.store.accessToken}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      placement,
+      reason,
+      postId,
+      comment
+    })
+  });
+
+  // if token expired, refresh it and retry
+  if (response.status === 401) {
+    await auth.refresh();
+    return postModerationReportsPost(placement, reason, postId, comment);
+  }
+
+  if (response.status !== 201) {
+    throw new BeRealError("Failed to report the post");
+  }
+
+  const json = await response.json() as {
+    reportId: string
+  };
+
+  return json.reportId;
+};
