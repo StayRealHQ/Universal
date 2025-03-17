2ec153eb532039b1cd20bdfdfa76101f49305fc5
Mikkel ALMONTE--RINGAUD <contact@vexcited.com>
2025-03-18 00:15:03 +0100

    chore(uno): ignore corvu type issue

--- uno.config.ts
+++ uno.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, presetWind3, transformerVariantGroup } from "unocss";
 import corvu from "@corvu/unocss";
 
 export default defineConfig({
+  // @ts-expect-error
   presets: [presetWind3(), corvu()],
   transformers: [transformerVariantGroup()]
 });
